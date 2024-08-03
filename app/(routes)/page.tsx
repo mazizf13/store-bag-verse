b5f3fc855513aaa8f70bcd04da0b3475246b8aea
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-08-03 10:30:12 +0700

    feat: product list

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,22 @@
 import getBanner from '@/actions/get-banner';
+import getProducts from '@/actions/get-products';
 import Banner from '@/components/banner';
+import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const banner = await getBanner('bd38a675-eee0-4c55-b49c-96f273d6cfa3');
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Banner data={banner} />
+        <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+          <ProductList title="Featured Product" items={products}/>
+        </div>
       </div>
     </Container>
   );
